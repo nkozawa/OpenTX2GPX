d31e965ec36bb7b95d9c1db177fc10b64c6a0385
Nobumichi <kozawa.n@gmail.com>
2021-12-12 16:23:45 +0800

    dev-srtFormat

--- OpenTX2SRT.py
+++ OpenTX2SRT.py
@@ -4,6 +4,8 @@ import csv
 import datetime
 import re
 
+from srtFormat import srtFormat
+
 flds = {}
 patDate = re.compile("\d{4}-\d{2}-\d{2}")
 patTime = re.compile("\d{2}:\d{2}:\d{2}\.\d+")
@@ -22,13 +24,14 @@ def main():
         crow.append([sg.Checkbox('',key='cItem'+str(i),enable_events=True)])
 
     for i in range(10):
-        ordrow.append([sg.Text('',key='otItem'+str(i)),sg.Button('^',key='bUp'+str(i),enable_events=True),sg.Button('v',key='bDown'+str(i),enable_events=True)])
+        ordrow.append([sg.Text('',size=(20,1),key='otItem'+str(i)),sg.Button('^',key='bUp'+str(i),enable_events=True),sg.Button('v',key='bDown'+str(i),enable_events=True)])
 
     layout = [
         [sg.Text('Pathname:'), sg.InputText(key='tPath', enable_events=True), sg.FileBrowse(file_types=(('CSV', '*.CSV'), ('CSV', '*.csv'),))],
         [sg.Text('OpenTX Log:', key='tLog'), sg.Button('Proceed', key='bProc')],
         [sg.Frame('Select(up to 10 items)', [[sg.Column(crow, size=(300,100), scrollable=True)]],key='fSel')],
         [sg.Frame('Order', ordrow)],
+        [sg.Frame('Output Format', [[sg.Text('',key='sFormat')]])],
         [sg.Text('Timestamp: YYYY MM DD', size=(21,1), pad=((5,0),(0,0))), sg.InputText(size=(4,1), key='iYYYY', pad=((0,2),(0,0))), sg.InputText(size=(2,1), key='iMM', pad=((0,2),(0,0))), sg.InputText(size=(2,1), key='iDD', pad=((0,10),(0,0))), sg.Text(' HHMMSS', size=(7,1), pad=((5,0),(0,0))), sg.InputText(size=(2,1), key='iHH', pad=((0,2),(0,0))), sg.InputText(size=(2,1), key='iMIN', pad=((0,2),(0,0))), sg.InputText(size=(2,1), key='iSS', pad=((0,2),(0,0))), sg.Button('Update')],
         [sg.Combo((), size=(30, 1), key='cLogSeq')]
     ]
@@ -40,7 +43,8 @@ def main():
         print(event, values)
         if event == sg.WIN_CLOSED:
             break
-        elif event == 'tPath':                      # line1 filename processing
+
+        elif event == 'tPath':                      # filename process
             print(">"+values['tPath']+"<")
             validLogFile = False
             window['tLog'].update("OpenTX Log: ")
@@ -52,27 +56,44 @@ def main():
                     print(otname)
                     window['tLog'].update("OpenTX Log: " + otname)
                     validLogFile = True
-        elif event == 'bProc':                      # line2 open log file
+        elif event == 'bProc':                      # open log file
             if (validLogFile):
                 print("opening log file...")
                 openTXLog(values['tPath'])
                 itemSelection(window)
-                window['iYYYY'].update(tdtFirst.strftime("%Y"))
-                window['iMM'].update(tdtFirst.strftime("%m"))
-                window['iDD'].update(tdtFirst.strftime("%d"))
-                window['iHH'].update(tdtFirst.strftime("%H"))
-                window['iMIN'].update(tdtFirst.strftime("%M"))
-                window['iSS'].update(tdtFirst.strftime("%S"))
                 logSeqUpdate(window)
         elif event[0:5] == 'cItem':                 # item selection
             itemSelectEvent(window,event,values)
             window['fSel'].update('Select(up to 10 items) Count:'+str(itemSelectCount))
+            outputFormat(window)
         elif event[0:3] == 'bUp':
             itemOrderUp(window,event)
+            outputFormat(window)
         elif event[0:5] == 'bDown':
             itemOrderDown(window,event)
+            outputFormat(window)
 
     window.close()
+
+def outputFormat(window):
+    sFormat = ""
+    for i in range(itemSelectCount):
+        logHeader = window['otItem'+str(i)].get()
+        if (logHeader == 'Date'):
+            print(srtFormat['Date'])
+            sFormat += logDT[i].strftime(srtFormat['Date']) + ' '
+        elif (logHeader == 'Time'):
+            sFormat += logDT[i].strftime(srtFormat['Time']) + ' '
+        else:
+            if (logHeader in srtFormat):
+                fmt = srtFormat[logHeader]
+            else:
+                fmt = '{0}'
+            if (fmt == ''):
+                sFormat += logData[0][flds[logHeader]] + ' '
+            else:
+                sFormat += fmt.format(logData[0][flds[logHeader]]) + ' '
+    window['sFormat'].update(sFormat)
 
 def itemOrderUp(window,event):
     i = int(event[3:len(event)])
@@ -121,6 +142,12 @@ def itemSelection(window):
             window['cItem'+str(i)].update(visible = False)
 
 def logSeqUpdate(window):
+    window['iYYYY'].update(tdtFirst.strftime("%Y"))
+    window['iMM'].update(tdtFirst.strftime("%m"))
+    window['iDD'].update(tdtFirst.strftime("%d"))
+    window['iHH'].update(tdtFirst.strftime("%H"))
+    window['iMIN'].update(tdtFirst.strftime("%M"))
+    window['iSS'].update(tdtFirst.strftime("%S"))
     logSeqTD = []
     totalDuration = logDT[len(logDT)-1] - logDT[0]
     if (len(logSeq) > 1):
@@ -153,7 +180,6 @@ def openTXLog(logfilename):
         openTXLog = csv.reader(f, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
         header = next(openTXLog)
         print(header)
-        #ヘッダー辞書を作りたい、
         i = 0
         for hdr in header:
             flds[hdr] = i
